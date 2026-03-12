d3bb74ba134a6864d11e4b2b5ab175efefc38619
John Ogletree <pxltr3@gmail.com>
2026-03-12 11:49:30 -0400

    Add Google Tag Manager/Analytics initialization script
    Initialize Google Tag Manager and Analytics.

---
+++ assets/js/gtag-init.js
@@ -0,0 +1,9 @@
+/**
+ * Google Tag Manager / Analytics Initialization
+ */
+window.dataLayer = window.dataLayer || [];
+function gtag(){
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-FY6WZRNX8M');
